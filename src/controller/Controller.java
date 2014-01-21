d0db2050c6993594252347c4fee632a41ef244bd
spencerd25 <spencerdavy@rocketmail.com>
2014-01-21 08:43:50 -0700

    add to repo

---
+++ src/controller/Controller.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javax.swing.*;
+
+public class Controller extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String args[])
+	{
+		JFrame Component = new JFrame();
+		Component.setVisible(true);
+		Component.setSize(500, 500);
+		Component.setLocation(400, 250);
+	}
+}
+
+
